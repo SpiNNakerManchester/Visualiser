e2d994fdf43db5ca1884523a0f1fc0beebf9d2fe
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2014-09-19 08:31:32 +0100

    Added a setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="Visualiser",
+    version="0.1-SNAPSHOT",
+    description="Spinnaker Visualiser Framework",
+    url="https://github.com/SpiNNakerManchester/Visualiser",
+    packages=['visualiser']
+)
