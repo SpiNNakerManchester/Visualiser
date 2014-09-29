02f849124896a6c38d06991c15a574e6fad3e205
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2014-09-29 12:28:04 +0100

    Updated windows file and instructions

--- scripts/virtualenv-gtk2.0-windows.bat
+++ scripts/virtualenv-gtk2.0-windows.bat
@@ -5,15 +5,15 @@ IF [%VIRTUAL_ENV%] == [] (
 )
 
 IF NOT EXIST "%VIRTUAL_ENV%"\Lib\site-packages\cairo (
-    mklink /D %VIRTUAL_ENV%\Lib\site-packages\cairo C:\Python2.7\Lib\site-packages\cairo\
+    mklink /D "%VIRTUAL_ENV%"\Lib\site-packages\cairo C:\Python27\Lib\site-packages\cairo
 )
 
 IF NOT EXIST "%VIRTUAL_ENV%"\Lib\site-packages\gtk-2.0 (
-    mklink /D %VIRTUAL_ENV%\Lib\site-packages\gtk-2.0 C:\Python2.7\Lib\site-packages\gtk-2.0\
+    mklink /D "%VIRTUAL_ENV%"\Lib\site-packages\gtk-2.0 C:\Python27\Lib\site-packages\gtk-2.0
 )
 
 IF NOT EXIST "%VIRTUAL_ENV%"\Lib\site-packages\pygtk.pth (
-    mklink %VIRTUAL_ENV%\Lib\site-packages\pygtk.pth C:\Python2.7\Lib\site-packages\pygtk.pth
+    mklink "%VIRTUAL_ENV%"\Lib\site-packages\pygtk.pth C:\Python27\Lib\site-packages\pygtk.pth
 )
 
 echo "Links for GTK have been created"
